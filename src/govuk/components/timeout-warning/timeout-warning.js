4b0025fdb17410fb8797419aeb4d33bac08d26b5
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2019-07-05 14:40:54 +0100

    Refactor JS
    ie9, safari, chrome work

--- src/govuk/components/timeout-warning/timeout-warning.js
+++ src/govuk/components/timeout-warning/timeout-warning.js
@@ -7,82 +7,105 @@ function TimeoutWarning ($module) {
   // this.$openButton = document.querySelector('.openModal')
   this.$closeButton = $module.querySelector('.js-dialog-close')
   this.$cancelButton = $module.querySelector('.js-dialog-cancel')
-  this.appOverlay = 'govuk-timeout-warning-overlay'
+  this.overLayClass = 'govuk-timeout-warning-overlay'
   this.$fallBackElement = document.querySelector('.govuk-timeout-warning-fallback')
+  // Does this need to be an array?
   this.timers = []
-  // Timer specific markup. If these are not present, timeout and redirection are disabled
-  this.$timer = $module.querySelector('#js-timeout-warning .timer')
-  this.$accessibleTimer = $module.querySelector('#js-timeout-warning .at-timer')
-  // Timer specific settings. If these are not set, timeout and redirection are disabled
-  this.idleMinutesBeforeTimeOut = $module.getAttribute('data-minutes-idle-timeout')
-  this.timeOutRedirectUrl = $module.getAttribute('data-url-redirect')
-  this.minutesTimeOutModalVisible = $module.getAttribute('data-minutes-modal-visible')
+  // UI countdown timer specific markup
+  this.$countdown = $module.querySelector('.timer')
+  this.$accessibleCountdown = $module.querySelector('.at-timer')
+  // UI countdown specific settings
+  this.idleMinutesBeforeTimeOut = $module.getAttribute('data-minutes-idle-timeout') ? $module.getAttribute('data-minutes-idle-timeout') : 25
+  this.timeOutRedirectUrl = $module.getAttribute('data-url-redirect') ? $module.getAttribute('data-url-redirect') : 'timeout'
+  this.minutesTimeOutModalVisible = $module.getAttribute('data-minutes-modal-visible') ? $module.getAttribute('data-minutes-modal-visible') : 5
   this.timeUserLastInteractedWithPage = ''
 }
 
-// Initialize component
+// Initialise component
 TimeoutWarning.prototype.init = function () {
   // Check for module
   if (!this.$module) {
     return
   }
 
-  if (this.$module) {
-    // Native dialog is not supported by browser so use polyfill
-    if (typeof HTMLDialogElement !== 'function') {
-      try {
-        window.dialogPolyfill.registerDialog(this.$module)
-      } catch (error) {
-        this.$fallBackElement.classList.add('govuk-!-display-block')
-        return
-      }
-    }
-    this.bindUIElements()
-    this.escClose()
+  // Check that dialog element has native or polyfill support
+  if (!this.dialogSupported()) {
+    return
+  }
 
-    if (this.isTimerSet()) {
-      this.idleTimeOut()
+  // Start watching for idleness
+  this.countIdleTime()
+
+  this.$closeButton.addEventListener('click', this.closeDialog.bind(this))
+  this.$module.addEventListener('keydown', this.escClose.bind(this))
+  // this.$cancelButton.addEventListener('click', this.disableBackButtonWhenOpen.bind(this))
+
+  // Debugging tip: This event doesn't kick in in Chrome if you have Inspector panel open and have clicked on it
+  // as it is now the active element. Click on the window to make it active before moving to another tab.
+  window.addEventListener('focus', this.checkIfShouldHaveTimedOut.bind(this))
+}
+
+TimeoutWarning.prototype.dialogSupported = function () {
+  if (typeof HTMLDialogElement === 'function') {
+    // Native dialog is supported by browser
+    return true
+  } else {
+    // Native dialog is not supported by browser so use polyfill
+    try {
+      window.dialogPolyfill.registerDialog(this.$module)
+      return true
+    } catch (error) {
+      // if ((typeof console !== 'undefined' || typeof console.log !== 'undefined')) {
+      //   console.log(error)
+      // }
+      // Doesn't support polyfill (IE8) - display fallback element
+      this.$fallBackElement.classList.add('govuk-!-display-block')
+      return false
     }
   }
 }
 
-TimeoutWarning.prototype.bindUIElements = function () {
-  // setTimeout(this.openDialog.bind(this), 6000) //debug
+// Count idle time (user not interacting with page)
+// Reset idle time counter when user interacts with the page
+// If user is idle for specified time period, open timeout warning as dialog
+TimeoutWarning.prototype.countIdleTime = function () {
+  var idleTime
+  var milliSecondsBeforeTimeOut = this.idleMinutesBeforeTimeOut * 60000
 
-  // this.$openButton.addEventListener('click', this.openDialog.bind(this))
+  // As user interacts with the page, keep resetting the timer
+  window.onload = resetIdleTime.bind(this)
+  window.onmousemove = resetIdleTime.bind(this)
+  window.onmousedown = resetIdleTime.bind(this) // Catches touchscreen presses
+  window.onclick = resetIdleTime.bind(this) // Catches touchpad clicks
+  window.onscroll = resetIdleTime.bind(this) // Catches scrolling with arrow keys
+  window.onkeypress = resetIdleTime.bind(this)
+  window.onkeyup = resetIdleTime.bind(this) // Catches Android keypad presses
 
-  this.$closeButton.addEventListener('click', this.closeDialog.bind(this))
+  function resetIdleTime () {
+    // As user has interacted with the page, reset idle time
+    clearTimeout(idleTime)
 
-  this.$module.addEventListener('keydown', this.escClose.bind(this))
+    // Start new idle time
+    idleTime = setTimeout(this.openDialog.bind(this), milliSecondsBeforeTimeOut)
 
-  // this.$cancelButton.on('click', function (e) {
-  //   e.preventDefault()
-  // })
-  // this.disableBackButtonWhenOpen()
-}
-
-TimeoutWarning.prototype.isDialogOpen = function () {
-  return this.$module['open']
-}
-
-TimeoutWarning.prototype.isTimerSet = function () {
-  return this.$timer &&
-         this.$accessibleTimer &&
-         this.idleMinutesBeforeTimeOut && this.minutesTimeOutModalVisible && this.timeOutRedirectUrl
+    // Store last interactive time for checkIfShouldHaveTimedOut()
+    // TO DO: tell server at intervals that user is active instead of storing last interaction time locally
+    if (window.localStorage) {
+      window.localStorage.setItem('timeUserLastInteractedWithPage', new Date())
+    }
+  }
 }
 
 TimeoutWarning.prototype.openDialog = function () {
-  // TO DO: get last interactive time from server to see if modal should be opened
+  // TO DO: get last interactive time from server to confirm dialog should be shown
   if (!this.isDialogOpen()) {
-    document.querySelector('body').classList.add(this.appOverlay)
+    document.querySelector('body').classList.add(this.overLayClass)
     this.saveLastFocusedEl()
     this.makePageContentInert()
     // polyfill?
     this.$module.showModal()
 
-    if (this.isTimerSet()) {
-      this.startTimer()
-    }
+    this.startUiCountdown()
 
     // if (window.history.pushState) {
     //   window.history.pushState('', '') // This updates the History API to enable state to be "popped" to detect browser navigation for disableBackButtonWhenOpen
@@ -90,27 +113,93 @@ TimeoutWarning.prototype.openDialog = function () {
   }
 }
 
-TimeoutWarning.prototype.closeDialog = function () {
-  if (this.isDialogOpen()) {
-    document.querySelector('body').classList.remove(this.appOverlay)
-    this.$module.close()
-    this.setFocusOnLastFocusedEl()
-    this.removeInertFromPageContent()
+// Starts a UI countdown timer. If timer is not cancelled before 0 reached + 4 seconds grace period, user is redirected.
+TimeoutWarning.prototype.startUiCountdown = function () {
+  this.clearTimers() // Clear any other modal timers that might have been running
+  var $module = this
+  var $countdown = this.$countdown
+  var $accessibleCountdown = this.$accessibleCountdown
+  var minutes = this.minutesTimeOutModalVisible
+  var timerRunOnce = false
+  var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
+  var timers = this.timers
 
-    if (this.isTimerSet()) {
-      this.clearTimers()
+  // TO DO: Contact server to find last active time, in case modal is open in another tab, and update time left here accordingly
+
+  var seconds = 60 * minutes
+
+  $countdown.innerHTML = minutes + ' minute' + (minutes > 1 ? 's' : '');
+
+  (function runTimer () {
+    var minutesLeft = parseInt(seconds / 60, 10)
+    var secondsLeft = parseInt(seconds % 60, 10)
+    var timerExpired = minutesLeft < 1 && secondsLeft < 1
+
+    var minutesText = minutesLeft > 0 ? '<span class="tabular-numbers">' + minutesLeft + '</span> minute' + (minutesLeft > 1 ? 's' : '') + '' : ' '
+    var secondsText = secondsLeft >= 1 ? ' <span class="tabular-numbers">' + secondsLeft + '</span> second' + (secondsLeft > 1 ? 's' : '') + '' : ''
+    var atMinutesNumberAsText = ''
+    var atSecondsNumberAsText = ''
+
+    try {
+      atMinutesNumberAsText = this.numberToWords(minutesLeft) // Attempt to convert numerics into text as iOS VoiceOver ccassionally stalled when encountering numbers
+      atSecondsNumberAsText = this.numberToWords(secondsLeft)
+    } catch (e) {
+      atMinutesNumberAsText = minutesLeft
+      atSecondsNumberAsText = secondsLeft
     }
-  }
-}
 
-TimeoutWarning.prototype.disableBackButtonWhenOpen = function () {
-  window.addEventListener('popstate', function () {
-    if (this.isDialogOpen()) {
-      this.closeDialog()
+    var atMinutesText = minutesLeft > 0 ? atMinutesNumberAsText + ' minute' + (minutesLeft > 1 ? 's' : '') + '' : ''
+    var atSecondsText = secondsLeft >= 1 ? ' ' + atSecondsNumberAsText + ' second' + (secondsLeft > 1 ? 's' : '') + '' : ''
+
+    // Below string will get read out by screen readers every time the timeout refreshes (every 15 secs. See below).
+    // Please add additional information in the modal body content or in below extraText which will get announced to AT the first time the time out opens
+    var text = 'We will reset your application if you do not respond in ' + minutesText + secondsText + '.'
+    var atText = 'We will reset your application if you do not respond in ' + atMinutesText
+    if (atSecondsText) {
+      if (minutesLeft > 0) {
+        atText += ' and'
+      }
+      atText += atSecondsText + '.'
     } else {
-      window.history.go(-1)
+      atText += '.'
     }
-  })
+    var extraText = ' We do this to keep your information secure.'
+
+    if (timerExpired) {
+      $countdown.innerHTML = 'You are about to be redirected'
+      $accessibleCountdown.innerHTML = 'You are about to be redirected'
+      //TO DO: tell server to reset userlastinteractedwithpage
+      if (window.localStorage) {
+        window.localStorage.setItem('timeUserLastInteractedWithPage', '')
+      }
+      setTimeout($module.redirect.bind($module), 4000)
+    } else {
+      seconds--
+
+      $countdown.innerHTML = text + extraText
+
+      if (minutesLeft < 1 && secondsLeft < 20) {
+        $accessibleCountdown.setAttribute('aria-live', 'assertive')
+      }
+
+      if (!timerRunOnce) {
+        // Read out the extra content only once. Don't read out on iOS VoiceOver which stalls on the longer text
+
+        if (iOS) {
+          $accessibleCountdown.innerHTML = atText
+        } else {
+          $accessibleCountdown.innerHTML = atText + extraText
+        }
+        timerRunOnce = true
+      } else if (secondsLeft % 15 === 0) {
+        // Update screen reader friendly content every 15 secs
+        $accessibleCountdown.innerHTML = atText
+      }
+
+      // JS doesn't allow resetting timers globally so timers need to be retained for resetting.
+      timers.push(setTimeout(runTimer, 1000))
+    }
+  })()
 }
 
 TimeoutWarning.prototype.saveLastFocusedEl = function () {
@@ -149,94 +238,18 @@ TimeoutWarning.prototype.removeInertFromPageContent = function () {
   }
 }
 
-// Starts a timer. If modal not closed before time out + 4 seconds grace period, user is redirected.
-TimeoutWarning.prototype.startTimer = function () {
-  this.clearTimers() // Clear any other modal timers that might have been running
-  var $module = this
-  var $timer = this.$timer
-  var $accessibleTimer = this.$accessibleTimer
-  var minutes = this.minutesTimeOutModalVisible
-  var timerRunOnce = false
-  var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
-  var timers = this.timers
+TimeoutWarning.prototype.isDialogOpen = function () {
+  return this.$module['open']
+}
 
-  if (minutes) {
-    // TO DO: Contact server to find last active time, in case modal is open in another tab, and update time left here accordingly
+TimeoutWarning.prototype.closeDialog = function () {
+  if (this.isDialogOpen()) {
+    document.querySelector('body').classList.remove(this.overLayClass)
+    this.$module.close()
+    this.setFocusOnLastFocusedEl()
+    this.removeInertFromPageContent()
 
-    var seconds = 60 * minutes
-
-    $timer.innerHTML = minutes + ' minute' + (minutes > 1 ? 's' : '');
-
-    (function runTimer () {
-      var minutesLeft = parseInt(seconds / 60, 10)
-      var secondsLeft = parseInt(seconds % 60, 10)
-      var timerExpired = minutesLeft < 1 && secondsLeft < 1
-
-      var minutesText = minutesLeft > 0 ? '<span class="tabular-numbers">' + minutesLeft + '</span> minute' + (minutesLeft > 1 ? 's' : '') + '' : ' '
-      var secondsText = secondsLeft >= 1 ? ' <span class="tabular-numbers">' + secondsLeft + '</span> second' + (secondsLeft > 1 ? 's' : '') + '' : ''
-      var atMinutesNumberAsText = ''
-      var atSecondsNumberAsText = ''
-
-      try {
-        atMinutesNumberAsText = this.numberToWords(minutesLeft) // Attempt to convert numerics into text as iOS VoiceOver ccassionally stalled when encountering numbers
-        atSecondsNumberAsText = this.numberToWords(secondsLeft)
-      } catch (e) {
-        atMinutesNumberAsText = minutesLeft
-        atSecondsNumberAsText = secondsLeft
-      }
-
-      var atMinutesText = minutesLeft > 0 ? atMinutesNumberAsText + ' minute' + (minutesLeft > 1 ? 's' : '') + '' : ''
-      var atSecondsText = secondsLeft >= 1 ? ' ' + atSecondsNumberAsText + ' second' + (secondsLeft > 1 ? 's' : '') + '' : ''
-
-      // Below string will get read out by screen readers every time the timeout refreshes (every 15 secs. See below).
-      // Please add additional information in the modal body content or in below extraText which will get announced to AT the first time the time out opens
-      var text = 'We will reset your application if you do not respond in ' + minutesText + secondsText + '.'
-      var atText = 'We will reset your application if you do not respond in ' + atMinutesText
-      if (atSecondsText) {
-        if (minutesLeft > 0) {
-          atText += ' and'
-        }
-        atText += atSecondsText + '.'
-      } else {
-        atText += '.'
-      }
-      var extraText = ' We do this to keep your information secure.'
-
-      if (timerExpired) {
-        $timer.innerHTML = 'You are about to be redirected'
-        $accessibleTimer.innerHTML = 'You are about to be redirected'
-        //TO DO: tell server to reset userlastinteractedwithpage
-        if (window.localStorage) {
-          window.localStorage.setItem('timeUserLastInteractedWithPage', '')
-        }
-        setTimeout($module.redirect, 4000)
-      } else {
-        seconds--
-
-        $timer.innerHTML = text + extraText
-
-        if (minutesLeft < 1 && secondsLeft < 20) {
-          $accessibleTimer.setAttribute('aria-live', 'assertive')
-        }
-
-        if (!timerRunOnce) {
-          // Read out the extra content only once. Don't read out on iOS VoiceOver which stalls on the longer text
-
-          if (iOS) {
-            $accessibleTimer.innerHTML = atText
-          } else {
-            $accessibleTimer.innerHTML = atText + extraText
-          }
-          timerRunOnce = true
-        } else if (secondsLeft % 15 === 0) {
-          // Update screen reader friendly content every 15 secs
-          $accessibleTimer.innerHTML = atText
-        }
-
-        // JS doesn't allow resetting timers globally so timers need to be retained for resetting.
-        timers.push(setTimeout(runTimer, 1000))
-      }
-    })()
+    this.clearTimers()
   }
 }
 
@@ -247,6 +260,16 @@ TimeoutWarning.prototype.clearTimers = function () {
   }
 }
 
+TimeoutWarning.prototype.disableBackButtonWhenOpen = function () {
+  window.addEventListener('popstate', function () {
+    if (this.isDialogOpen()) {
+      this.closeDialog()
+    } else {
+      window.history.go(-1)
+    }
+  })
+}
+
 // Close modal when ESC pressed
 TimeoutWarning.prototype.escClose = function (event) {
   // get the target element
@@ -255,60 +278,26 @@ TimeoutWarning.prototype.escClose = function (event) {
   }
 }
 
-TimeoutWarning.prototype.idleTimeOut = function () {
-  var idleMinutes = this.idleMinutesBeforeTimeOut
-  var milliSeconds
-  var timer
-
-  this.checkIfShouldHaveTimedOut()
-
-  if (idleMinutes) {
-    milliSeconds = idleMinutes * 60000
-
-    window.onload = resetTimer.bind(this)
-    window.onmousemove = resetTimer.bind(this)
-    window.onmousedown = resetTimer.bind(this) // Catches touchscreen presses
-    window.onclick = resetTimer.bind(this) // Catches touchpad clicks
-    window.onscroll = resetTimer.bind(this) // Catches scrolling with arrow keys
-    window.onkeypress = resetTimer.bind(this)
-    window.onkeyup = resetTimer.bind(this) // Catches Android keypad presses
-  }
-
-  function resetTimer () {
-    clearTimeout(timer)
-
-    // TO DO: tell server at intervals that user is active instead of storing last interaction time locally
-    if (window.localStorage) {
-      window.localStorage.setItem('timeUserLastInteractedWithPage', new Date())
-    }
-
-    timer = setTimeout(this.openDialog.bind(this), milliSeconds)
-  }
-}
-
-// Do a timestamp comparison when the page regains focus to check if the user should have been timed out already
+// Do a timestamp comparison with server when the page regains focus to check
+// if the user should have been timed out already.
+// This could happen but because the computer went to sleep, the browser crashed etc.
 TimeoutWarning.prototype.checkIfShouldHaveTimedOut = function () {
-  window.onfocus = function () {
-    // Debugging tip: Above event doesn't kick in in Chrome if you have Inspector panel open and have clicked on it
-    // as it is now the active element. Click on the window to make it active before moving to another tab.
-    if (window.localStorage) {
-      // TO DO: timeUsexrLastInteractedWithPage should in fact come from the server
-      var timeUserLastInteractedWithPage = new Date(window.localStorage.getItem('timeUserLastInteractedWithPage'))
+  if (window.localStorage) {
+    // TO DO: timeUsexrLastInteractedWithPage should in fact come from the server
+    var timeUserLastInteractedWithPage = new Date(window.localStorage.getItem('timeUserLastInteractedWithPage'))
 
-      var seconds = Math.abs((timeUserLastInteractedWithPage - new Date()) / 1000)
+    var seconds = Math.abs((timeUserLastInteractedWithPage - new Date()) / 1000)
 
-      // TO DO: use both idlemin and timemodalvisible
-      if (seconds > this.idleMinutesBeforeTimeOut * 60) {
-
-    //  if (seconds > 60) {
-        this.redirect()
-      }
-
-      //   // TO DO: open modal if advised so by the server, tell modal how many seconds are left
-      // else if () {
-      //   this.openDialog.bind(this)
-      // }
+    // TO DO: use both idlemin and timemodalvisible
+    if (seconds > this.idleMinutesBeforeTimeOut * 60) {
+      // if (seconds > 60) {
+      this.redirect.bind(this)
     }
+
+    //   // TO DO: open modal if advised so by the server, tell modal how many seconds are left
+    // else if () {
+    //   this.openDialog.bind(this)
+    // }
   }
 }
 TimeoutWarning.prototype.redirect = function () {
