f486e68b6423aa49c9df6958499710c92a1833dd
Syed Ahmad <syed-ahmad@outlook.com>
2019-07-29 16:31:45 +0100

    Prettify all.test.js

--- src/govuk/all.test.js
+++ src/govuk/all.test.js
@@ -1,45 +1,53 @@
 /* eslint-env jest */
 
-const configPaths = require('../../config/paths.json')
-const PORT = configPaths.ports.test
+const configPaths = require('../../config/paths.json');
+const PORT = configPaths.ports.test;
 
-const { renderSass } = require('../../lib/jest-helpers')
+const { renderSass } = require('../../lib/jest-helpers');
 
-let baseUrl = 'http://localhost:' + PORT
+let baseUrl = 'http://localhost:' + PORT;
 
-beforeAll(async (done) => {
+beforeAll(async done => {
   // Capture JavaScript errors.
   page.on('pageerror', error => {
     // If the stack trace includes 'all.js' then we want to fail these tests.
     if (error.stack.includes('all.js')) {
-      throw error
+      throw error;
     }
-  })
-  done()
-})
+  });
+  done();
+});
 
 describe('GOV.UK Frontend', () => {
   describe('javascript', () => {
     it('can be accessed via `GOVUKFrontend`', async () => {
-      await page.goto(baseUrl + '/', { waitUntil: 'load' })
+      await page.goto(baseUrl + '/', { waitUntil: 'load' });
 
-      const GOVUKFrontendGlobal = await page.evaluate(() => window.GOVUKFrontend)
+      const GOVUKFrontendGlobal = await page.evaluate(
+        () => window.GOVUKFrontend
+      );
 
-      expect(typeof GOVUKFrontendGlobal).toBe('object')
-    })
+      expect(typeof GOVUKFrontendGlobal).toBe('object');
+    });
     it('exports `initAll` function', async () => {
-      await page.goto(baseUrl + '/', { waitUntil: 'load' })
+      await page.goto(baseUrl + '/', { waitUntil: 'load' });
 
-      const typeofInitAll = await page.evaluate(() => typeof window.GOVUKFrontend.initAll)
+      const typeofInitAll = await page.evaluate(
+        () => typeof window.GOVUKFrontend.initAll
+      );
 
-      expect(typeofInitAll).toEqual('function')
-    })
+      expect(typeofInitAll).toEqual('function');
+    });
     it('exports Components', async () => {
-      await page.goto(baseUrl + '/', { waitUntil: 'load' })
+      await page.goto(baseUrl + '/', { waitUntil: 'load' });
 
-      const GOVUKFrontendGlobal = await page.evaluate(() => window.GOVUKFrontend)
+      const GOVUKFrontendGlobal = await page.evaluate(
+        () => window.GOVUKFrontend
+      );
 
-      var components = Object.keys(GOVUKFrontendGlobal).filter(method => method !== 'initAll')
+      var components = Object.keys(GOVUKFrontendGlobal).filter(
+        method => method !== 'initAll'
+      );
 
       // Ensure GOV.UK Frontend exports the expected components
       expect(components).toEqual([
@@ -52,66 +60,72 @@ describe('GOV.UK Frontend', () => {
         'Header',
         'Radios',
         'Tabs'
-      ])
-    })
+      ]);
+    });
     it('exported Components can be initialised', async () => {
-      await page.goto(baseUrl + '/', { waitUntil: 'load' })
+      await page.goto(baseUrl + '/', { waitUntil: 'load' });
 
-      const GOVUKFrontendGlobal = await page.evaluate(() => window.GOVUKFrontend)
+      const GOVUKFrontendGlobal = await page.evaluate(
+        () => window.GOVUKFrontend
+      );
 
-      var components = Object.keys(GOVUKFrontendGlobal).filter(method => method !== 'initAll')
+      var components = Object.keys(GOVUKFrontendGlobal).filter(
+        method => method !== 'initAll'
+      );
 
       // Check that all the components on the GOV.UK Frontend global can be initialised
       components.forEach(component => {
         page.evaluate(component => {
-          const Component = window.GOVUKFrontend[component]
-          const $module = document.documentElement
-          new Component($module).init()
-        }, component)
-      })
-    })
+          const Component = window.GOVUKFrontend[component];
+          const $module = document.documentElement;
+          new Component($module).init();
+        }, component);
+      });
+    });
     it('can be initialised scoped to certain sections of the page', async () => {
-      await page.goto(baseUrl + '/examples/scoped-initialisation', { waitUntil: 'load' })
+      await page.goto(baseUrl + '/examples/scoped-initialisation', {
+        waitUntil: 'load'
+      });
 
       // To test that certain parts of the page are scoped we have two similar components
       // that we can interact with to check if they're interactive.
 
       // Check that the conditional reveal component has a conditional section that would open if enhanced.
-      await page.waitForSelector('#conditional-not-scoped', { hidden: true })
+      await page.waitForSelector('#conditional-not-scoped', { hidden: true });
 
-      await page.click('[for="not-scoped"]')
+      await page.click('[for="not-scoped"]');
 
       // Check that when it is clicked that nothing opens, which shows that it has not been enhanced.
-      await page.waitForSelector('#conditional-not-scoped', { hidden: true })
+      await page.waitForSelector('#conditional-not-scoped', { hidden: true });
 
       // Check the other conditional reveal which has been enhanced based on it's scope.
-      await page.waitForSelector('#conditional-scoped', { hidden: true })
+      await page.waitForSelector('#conditional-scoped', { hidden: true });
 
-      await page.click('[for="scoped"]')
+      await page.click('[for="scoped"]');
 
       // Check that it has opened as expected.
-      await page.waitForSelector('#conditional-scoped', { hidden: false })
-    })
-  })
+      await page.waitForSelector('#conditional-scoped', { hidden: false });
+    });
+  });
   describe('global styles', () => {
     it('are disabled by default', async () => {
       const sass = `
         @import "all";
-      `
-      const results = await renderSass({ data: sass })
-      expect(results.css.toString()).not.toContain(', a {')
-      expect(results.css.toString()).not.toContain(', p {')
-    })
+      `;
+      const results = await renderSass({ data: sass });
+      expect(results.css.toString()).not.toContain(', a {');
+      expect(results.css.toString()).not.toContain(', p {');
+    });
     it('are enabled if $global-styles variable is set to true', async () => {
       const sass = `
         $govuk-global-styles: true;
         @import "all";
-      `
-      const results = await renderSass({ data: sass })
-      expect(results.css.toString()).toContain(', a {')
-      expect(results.css.toString()).toContain(', p {')
-    })
-  })
+      `;
+      const results = await renderSass({ data: sass });
+      expect(results.css.toString()).toContain(', a {');
+      expect(results.css.toString()).toContain(', p {');
+    });
+  });
 
   // Sass functions will be automatically evaluated at compile time and the
   // return value from the function will be used in the compiled CSS.
@@ -144,13 +158,13 @@ describe('GOV.UK Frontend', () => {
   // the compiled CSS - if it finds anything, it will result in the test
   // failing.
   it('does not contain any unexpected govuk- function calls', async () => {
-    const sass = `@import "all"`
+    const sass = `@import "all"`;
 
-    const results = await renderSass({ data: sass })
-    const css = results.css.toString()
+    const results = await renderSass({ data: sass });
+    const css = results.css.toString();
 
-    const functionCalls = css.match(/_?govuk-[\w-]+\(.*?\)/g)
+    const functionCalls = css.match(/_?govuk-[\w-]+\(.*?\)/g);
 
-    expect(functionCalls).toBeNull()
-  })
-})
+    expect(functionCalls).toBeNull();
+  });
+});
